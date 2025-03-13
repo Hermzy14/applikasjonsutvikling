10e160cbaa6b0888b5de93b1ec746100b67cd764
Herman Lundby-Holen <hermanluho@gmail.com>
2025-03-13 11:04:35 +0100

    Added todo to add a reference to users favorite courses later on.

--- src/main/java/no/ntnu/iir/idata/gr9/backend/entity/User.java
+++ src/main/java/no/ntnu/iir/idata/gr9/backend/entity/User.java
@@ -18,6 +18,7 @@ public class User {
   private String email;
   private String password;
   private boolean isAdmin;
+  // TODO: Add a field to the users favorite courses.
 
   public User() {
   }
