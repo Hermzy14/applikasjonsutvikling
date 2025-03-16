0889acf6e6a9aa75fd83baf88eba43b95beeb290
Herman Lundby-Holen <hermanluho@gmail.com>
2025-03-16 16:57:47 +0100

    Added a Course class.

---
+++ src/main/java/no/ntnu/iir/idata/gr9/backend/entity/Course.java
@@ -0,0 +1,37 @@
+package no.ntnu.iir.idata.gr9.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+/**
+ * Represents a course.
+ * <p>A course includes one of the following categories:</p>
+ * <ul>
+ *   <li>Information Technologies</li>
+ *   <li>Digital Marketing</li>
+ *   <li>Business and Entrepreneurship</li>
+ *   <li>Data Science and Analytics</li>
+ * </ul>
+ * <p>Each course has:</p>
+ * <ul>
+ *   <li>An id</li>
+ *   <li>A title</li>
+ *   <li>A description</li>
+ *   <li>A category</li>
+ *   <li>Keywords</li>
+ *   <li>A level of difficulty</li>
+ *   <li>A course session/duration</li>
+ *   <li>ECTS credits</li>
+ *   <li>Hours per week</li>
+ *   <li>Related certifications</li>
+ *   <li>Course providers who can have different pricing for the course</li>
+ * </ul>
+ */
+@Entity
+public class Course {
+  @Id
+  private int id;
+
+  public Course() {}
+
+}
