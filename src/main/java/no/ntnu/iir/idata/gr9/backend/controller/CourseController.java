b581ba796c566910175a654c8a22438c6895a946
Herman Lundby-Holen <hermanluho@gmail.com>
2025-03-28 13:41:42 +0100

    Created CourseController class.

---
+++ src/main/java/no/ntnu/iir/idata/gr9/backend/controller/CourseController.java
@@ -0,0 +1,12 @@
+package no.ntnu.iir.idata.gr9.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST API controller for managing courses.
+ */
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+}
