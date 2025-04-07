47fa528112b4fec3cf7ad7ed8e9a7e0be733bee3
MilkDirt <melkior@outlook.com>
2025-04-07 18:41:33 +0200

    did some stuff on user controller

--- src/main/java/no/ntnu/iir/idata/gr9/backend/controller/UserController.java
+++ src/main/java/no/ntnu/iir/idata/gr9/backend/controller/UserController.java
@@ -13,12 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 /**
@@ -28,4 +23,26 @@ import org.springframework.web.server.ResponseStatusException;
 @RequestMapping("/users")
 public class UserController {
 private final UserRepository userRepository;
+private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+/**
+ * constructor for UserController.
+ *
+ * @param userRepository the repository for managing users
+ */
+public UserController(UserRepository userRepository) {
+    this.userRepository = userRepository;
+}
+
+/**
+ * Register a new user.
+ * <p>
+ * Endpoint: {@code POST /users/register}.
+ *
+ * @param user the user to register
+ */
+@PostMapping("/register")
+
+
+
 }
